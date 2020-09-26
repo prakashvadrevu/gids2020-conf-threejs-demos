6361630c5798579c78513eaa0a7ae41480b1a8b1
Prakash Vadrevu <prakash.vadrevu@hotmail.com>
2020-09-27 04:46:00 +0530

    Hack to address ground floor and table not being rendered in the scene

--- src/scene-loader.js
+++ src/scene-loader.js
@@ -20,6 +20,13 @@ function loadScene(gltf, scene) {
       })
     }
   });
+
+  // TODO: For some reason, both `ground_floor` and `table` are being skipped in the above mapping. So, process them here.
+  let groundFloor = gltf.scene.children.filter(c => c.name === "ground_floor")[0];
+  convertToGeometry(groundFloor, groundFloor, scene);
+
+  let table = gltf.scene.children.filter(c => c.name === "table")[0];
+  convertToGeometryAndLoadChildrenToScene(table, table, scene);
 }
 
 function convertToGeometryAndLoadChildrenToScene(object, parentObject, scene) {
