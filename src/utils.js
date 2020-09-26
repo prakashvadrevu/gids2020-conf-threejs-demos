c2e3138b456956c98f1ffa3140e3c2ab3cf7e4ec
Prakash Vadrevu <prakash.vadrevu@hotmail.com>
2020-09-26 13:45:00 +0530

    refactor: move plane and lights to utils

--- src/utils.js
+++ src/utils.js
@@ -44,6 +44,56 @@ function addGroundPlane(scene) {
     return plane;
 }
 
+function addPlaneAndLights(scene) {
+    let planeGeometry = new THREE.PlaneGeometry(1000, 1000, 20, 20);
+    let planeMaterial = new THREE.MeshLambertMaterial();
+    let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.receiveShadow = true;
+
+    // rotate and position the plane
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+
+    plane.name = "ground";
+
+    addLights(plane, scene);
+
+    // add the plane to the scene
+    scene.add(plane);
+}
+
+function addLights(plane, scene) {
+    // add spotlight for a bit of light
+    let spotLight0 = new THREE.SpotLight(0xcccccc);
+    spotLight0.position.set(-40, 60, -10);
+    spotLight0.lookAt(plane);
+    scene.add(spotLight0);
+
+    let target = new THREE.Object3D();
+    target.position = new THREE.Vector3(5, 0, 0);
+
+    let hemiLight = new THREE.HemisphereLight(0x0000ff, 0x00ff00, 0.6);
+    hemiLight.position.set(0, 500, 0);
+    scene.add(hemiLight);
+
+    let pointColor = "#ffffff";
+    let dirLight = new THREE.DirectionalLight(pointColor);
+    dirLight.position.set(30, 10, 50);
+    dirLight.castShadow = true;
+    dirLight.target = plane;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 200;
+    dirLight.shadow.camera.left = -50;
+    dirLight.shadow.camera.right = 50;
+    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.bottom = -50;
+    dirLight.shadow.mapSize.width = 2048;
+    dirLight.shadow.mapSize.height = 2048;
+    scene.add(dirLight);
+}
+
 function initTrackballControls(camera, renderer) {
     console.log("initTrackballControls");
 
