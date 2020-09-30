916d64587e86e2bc9d2244ed856760e2fd43402c
Prakash Vadrevu <prakash.vadrevu@hotmail.com>
2020-09-30 09:13:04 +0530

    Fix bug in csg-cut: showFullView toggling every child's visibility to true

--- src/csg-cut.js
+++ src/csg-cut.js
@@ -1,17 +1,22 @@
 
+let list = [
+    "side_floor_top--side_floor_top",
+    "first_floor_rear_wall_top--first_floor_rear_wall_top",
+    "first_floor_top--first_floor_top",
+    "first_floor_filler_top--first_floor_filler_top"
+];
+
 function showCrossSectionalView(scene) {
-    let list = [
-        "side_floor_top--side_floor_top",
-        "first_floor_rear_wall_top--first_floor_rear_wall_top",
-        "first_floor_top--first_floor_top",
-        "first_floor_filler_top--first_floor_filler_top"
-    ];
-    list.map(name => {
-        let child = scene.children.filter(c => c.name === name)[0];
-        child.visible = false
-    });
+    showTopElements(scene, false);
 }
 
 function showFullView(scene) {
-    scene.children.map(e => e.visible = true);
+    showTopElements(scene, true);
+}
+
+function showTopElements(scene, toggle) {
+    list.map(name => {
+        let child = scene.children.filter(c => c.name === name)[0];
+        child.visible = toggle;
+    });
 }
